a4181536a8d33cf9d4140f3cf972a0f1f004a8e1
Kons <konss97@gmail.com>
2021-06-18 17:47:19 +0200

    Initial commit

---
+++ bin/lww_crdt.dart
@@ -0,0 +1,5 @@
+import 'package:lww_crdt/lww_crdt.dart' as lww_crdt;
+
+void main(List<String> arguments) {
+  print('Hello world: ${lww_crdt.calculate()}!');
+}
---
+++ test/lww_crdt_test.dart
@@ -0,0 +1,8 @@
+import 'package:lww_crdt/lww_crdt.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
